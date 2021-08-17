4663c9704951e6bd0858590bce66ca2f92639575
AdrianKorkus <adrian.korkus.2017@gmail.com>
2021-08-17 21:31:45 +0200

    Add plotly with salaries.

---
+++ matplot.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as ploter
+
+salaries = [
+    ("Mark", 1000),
+    ("John", 1500),
+    ("Daniel", 2300),
+    ("Greg", 5000)
+]
+
+
+X = [0,1,2,3]
+Y = [0,1,0,1]
+ploter.plot(X, Y)
